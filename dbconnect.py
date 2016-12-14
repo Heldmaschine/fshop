b7c173c76085da77268f4700d7abbf8f1f03ec44
whereeaglesdare <belokur.bogdan@yandex.ru>
2016-12-14 18:18:11 +0200

    Delete dbconnect.py

--- dbconnect.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-def psql_connect():
-    conn_string = "host='localhost' dbname='test1' user='testuser' password='yadbonusy'"
-    conn = psycopg2.connect(conn_string)
-    cur = conn.cursor()
-    return cur, conn
-
